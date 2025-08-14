e71bd7a428d069354f04fa20d4a8f0a90ede6526
Abraão Ribeiro <abraaobeiro@gmail.com>
2025-08-14 14:45:03 -0300

    feat: add footer

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-gradient-to-r from-blue-900 to-indigo-900 border-t-4 border-yellow-500 py-8">
+    <div class="container mx-auto px-4">
+        <div class="text-center text-gray-400 text-sm">
+            © 2023 Clash of Clans Brasil. Todos os direitos reservados.
+        </div>
+    </div>
+</footer>
